36fe38c3962aba966011b3174912b0718f2e6ba1
mmarmdq <maar.mdq96@hotmail.com>
2021-03-16 19:50:03 -0300

    asdasd

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        holi("mocho");
+
+    }
+
+    public static void holi(String args) {
+        System.out.println(args);
+        System.out.println(" se llamo a holi");
+    }
+}
